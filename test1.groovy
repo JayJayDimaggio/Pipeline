a5de7e7eb077a906c79042bb67e42f1235a4fc37
JayJayDimaggio <darkprincepower@gmail.com>
2020-04-16 16:23:07 +0300

    upload jenkins file

---
+++ test1.groovy
@@ -0,0 +1,24 @@
+pipleine {
+		agent any
+				stages{
+						stage('One') {
+								steps {
+									echo 'Hi, This is Felix Vainer'
+								}
+						}
+						stage('Two') {
+								steps {
+									input ('Do you want to proceed?')
+								}
+						}	
+						stage('Three') {
+								when {
+										not {
+												branch "master"
+										}
+								}
+								steps {
+										echo "Hello from step 3"
+								}
+						}													
+}
